7e765b7e0b88fb5d6530dde8334aa191b20c3993
IvayloSilovski1 <ivaylo.silovski689@gmail.com>
2020-03-29 14:03:55 +0200

    first commit

---
+++ models/shorturl.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+
+const ShortUrlSchema = new mongoose.Schema({
+
+    fullUrl:{
+        type: String,
+        required: true
+    },
+    shortUrl: {
+        type: String,
+        required: true,
+        default: shortid.generate
+    },
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+
+});
+
+
+module.exports = mongoose.model('ShortUrl', ShortUrlSchema);
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shorturl');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+mongoose.connect('mongodb://localhost/urlshortener', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+app.get('/', async (req, res) => {
+    const shorturls = await ShortUrl.find();
+    res.render('index', { shorturls: shorturls });
+});
+
+
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({
+        fullUrl: req.body.fullUrl
+    });
+    res.redirect('/');
+});
+
+
+app.get('/:shortUrl', async (req, res) => {
+    const shorturl = await ShortUrl.findOne({ shortUrl: req.params.shortUrl });
+    if(shorturl == null) return res.sendStatus(404);
+
+    shorturl.clicks++;
+    shorturl.save();
+    res.redirect(shorturl.fullUrl)
+});
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on http://localhost:${port}`));
